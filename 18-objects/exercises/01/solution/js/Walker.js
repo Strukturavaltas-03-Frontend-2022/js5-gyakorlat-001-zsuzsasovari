4dcbf7bfac76978f1d404c4874f70f231593bbdb
zsuzsasovari <105643090+zsuzsasovari@users.noreply.github.com>
2022-09-13 20:46:16 +0200

    kész

--- 18-objects/exercises/01/solution/js/Walker.js
+++ 18-objects/exercises/01/solution/js/Walker.js
@@ -1,1 +1,38 @@
+function Walker (name = 'John Doe', state = 'dead') {
+    this.name = name;
+    this.state = state;
+    
+    Object.defineProperty(this, 'firstName', {
+        get () {
+            return this.name.split(' ')[0];
+        },
+
+        set (value){
+            this.name = `${value} ${this.name.split(' ')[1]}`;
+        }
+    });
+
+    Object.defineProperty(this, 'lastName',{
+        get () {
+            return this.name.split(' ')[1];
+        },
+
+        set (value){
+            this.name = `${this.name.split(' ')[0]} ${value}`;
+        }
+    });
+}
+
+Walker.prototype.walk = function (speedInMph = '10'){
+    return `${this.name} the ${this.state} walks with ${speedInMph} mph!`;
+};
+
+Walker.prototype.eat = function(foodName = 'meat') {
+    return `${this.name} the ${this.state} eats ${foodName}!`;
+};
+
+Walker.prototype.speak = function(sound = 'grrrrrr'){
+    return `${this.name} the ${this.state} says ${sound}!`;
+};
+
 export default Walker;
